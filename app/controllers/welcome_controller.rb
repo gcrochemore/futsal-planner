8cf1f301aff19fc7cd0a30db11ec5897c0482160
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2017-05-30 21:41:00 +0200

    feat(homepage): add homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def show
+    @futsal_games = FutsalGame.all
+    @q = Goal.accessible_by(current_ability).ransack(params[:q])
+    @goals = @q.result
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+  root 'welcome#show'
 
   resources :paper_trail_versions, controller: 'versions'
   resources :companies, :futsal_fields, :game_registrations, :teams, :highlights, 
