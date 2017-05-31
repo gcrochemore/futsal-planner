cd877daf21acaed3109d85f3e9b37e3f2809c0d6
gcrochemore <guillaume.crochemore@trsb.net>
2017-05-31 09:49:23 +0200

    feat(homepage): fix bug on heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
 
   def show_player user 
     tooltip = '<span class="label-tooltip drop-menu">' + (link_to icon('user-circle-o') + " " +  user, user, target: "_blank") +'
-                <span class="label-tooltiptext  label-tooltip-bottom">' + user.display_stats + '</span>
+                <span class="label-tooltiptext  label-tooltip-bottom">' + user.andand.display_stats + '</span>
               </span>'
     tooltip.html_safe
   end 
