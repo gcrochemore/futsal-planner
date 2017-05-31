27fb4c2d77590c5f5a6b218e1a6f11b181b86769
gcrochemore <guillaume.crochemore@trsb.net>
2017-05-31 18:04:32 +0200

    fix(welcomepage): fix error if load goal and assist

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
   end
   def show
-    @futsal_games = FutsalGame.includes(:team_home).includes(:team_outside).all
-    @q = Goal.includes(:goal).includes(:assist).accessible_by(current_ability).ransack(params[:q])
+    @futsal_games = FutsalGame.joins(:team_home, :team_outside).all
+    @q = Goal.joins(:goal, :assist).accessible_by(current_ability).ransack(params[:q])
     @goals = @q.result
   end
 end
