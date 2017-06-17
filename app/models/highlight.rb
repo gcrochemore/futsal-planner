10bf8f32fa144178907d6e12bd894541578b3d6a
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2017-06-17 14:35:10 +0200

    fix(data): import error

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -4,6 +4,6 @@ class Highlight < ApplicationRecord
   belongs_to :highlight_type
   belongs_to :futsal_game
   belongs_to :team
-  belongs_to :author, :class_name => :User,:foreign_key => "author_id"
-  belongs_to :victim, :class_name => :User,:foreign_key => "victim_id"
+  belongs_to :author, :class_name => :User,:foreign_key => "author_id", optional: true
+  belongs_to :victim, :class_name => :User,:foreign_key => "victim_id", optional: true
 end
