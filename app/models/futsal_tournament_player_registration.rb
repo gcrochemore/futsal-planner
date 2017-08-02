a3d2ecb8f05aae70bde23de62a23f926ec7c9a3b
Guillaume Crochemore <guillaume.crochemore@gmail.com>
2017-08-02 13:52:24 +0200

    feat(data): improve stats calculation

--- app/models/futsal_tournament_player_registration.rb
+++ app/models/futsal_tournament_player_registration.rb
@@ -19,9 +19,9 @@ class FutsalTournamentPlayerRegistration < ApplicationRecord
 
     futsal_game_duration = (self.futsal_tournament.duration * 60)
 
-    self.goalkeeper_duration = FutsalGamePlayerPosition.where('futsal_position_id = ? AND game_registration_id IN (?)', 1, self.futsal_tournament.game_registration_by_user(self.user).pluck(:id)).sum(:duration)
-    self.substitute_duration = FutsalGamePlayerPosition.where('futsal_position_id = ? AND game_registration_id IN (?)', 100, self.futsal_tournament.game_registration_by_user(self.user).pluck(:id)).sum(:duration)
-    self.player_duration = futsal_game_duration - self.goalkeeper_duration - self.substitute_duration
+    self.goalkeeper_duration = self.futsal_tournament.game_registration_by_user(self.user).sum(:goalkeeper_duration)
+    self.substitute_duration = self.futsal_tournament.game_registration_by_user(self.user).sum(:substitute_duration)
+    self.player_duration = self.futsal_tournament.game_registration_by_user(self.user).sum(:player_duration)
     
     self.goalkeeper_goal_against_average = 0
 
