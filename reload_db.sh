90a875d6e420305fdca330e8f908686a1b494e89
gcrochemore <guillaume.crochemore@trsb.net>
2019-04-01 18:45:00 +0200

    feat(data): add last match

--- reload_db.sh
+++ reload_db.sh
@@ -1,3 +1,5 @@
+docker-compose stop
 docker-compose run futsal-planner-front rake db:drop
 docker-compose run futsal-planner-front rake db:create
 docker-compose run futsal-planner-front rake db:migrate
+docker-compose up -d
